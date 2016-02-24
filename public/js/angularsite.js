f27f6f7027481cb9a684bb79f16298992e5c524a
Nipuna Bhotla <nipunab@techaspect.com>
2016-02-24 19:59:22 +0530

    added blog and discussion pages

--- public/js/angularsite.js
+++ public/js/angularsite.js
@@ -1,11 +1,15 @@
-var siteApp = angular.module('siteApp', ['ngRoute']);
+var siteApp = angular.module('siteApp', ['ngRoute','firebase']);
 siteApp.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/home', {templateUrl: 'partials/HomePage.html', controller: 'HomePageController'})
          .when('/projects', {templateUrl: 'partials/Projects.html', controller: 'ProjectsController'})
+         // .when('/blogs', {templateUrl: 'partials/Blog.html', controller: 'BlogsController'})
+         .when('/discussions', {templateUrl: 'partials/Discussions.html', controller: 'DiscussionsController'})
           .when('/coe', {templateUrl: 'partials/COE.html', controller: 'COEController'})
            .when('/trainings', {templateUrl: 'partials/Trainings.html', controller: 'TrainingsController'})
+          .when('/projects/blogs', {templateUrl: 'partials/Blog.html', controller: 'BlogsController'})
            .when('/projects/:projectname', {templateUrl: 'partials/ProjectDetails.html', controller: 'ProjetListController'})
+
         .otherwise({redirectTo: '/home'});   
 }]);
 siteApp.controller('HomePageController',function($scope){
@@ -15,12 +19,51 @@ siteApp.controller('ProjetListController',function($scope,$routeParams){
 			console.log("selected project is ",$routeParams.projectname);
 		});
 siteApp.controller('ProjectsController',function($scope){
+  
 			 $scope.IsProjectVisible = false;
             $scope.ShowProjectList = function () {
                 //If DIV is visible it will be hidden and vice versa.
                 $scope.IsProjectVisible = $scope.IsProjectVisible ? false : true;
             }
+           
 		});
+siteApp.controller('BlogsController',function($scope){
+  
+          });
+siteApp.controller('DiscussionsController', ['$scope','Message',function($scope,Message,$routeParams){ 
+       
+   $scope.user="Guest";
+ 
+      $scope.messages= Message.all;
+
+      $scope.inserting = function(message){
+        Message.create(message);
+      };
+  }]);
+siteApp.factory('Message', ['$firebase',
+  function($firebase) {
+    var ref = new Firebase('https://tachatapp.firebaseIO.com');
+    var messages = $firebase(ref.child('messages')).$asArray();
+
+    var Message = {
+      all: messages,
+      create: function (message) {
+        return messages.$add(message);
+      },
+      get: function (messageId) {
+        return $firebase(ref.child('messages').child(messageId)).$asObject();
+      },
+      delete: function (message) {
+        return messages.$remove(message);
+      }
+    };
+
+    return Message;
+
+  }
+  ]);
+
+ 
 siteApp.controller('COEController',function($scope){ 
 			 $scope.IsCOEVisible = false;
             $scope.ShowCOEList = function () {
@@ -35,3 +78,13 @@ siteApp.controller('TrainingsController',function($scope){
                 $scope.TrainingsVisible = $scope.TrainingsVisible ? false : true;
             }
 		});
+
+siteApp.directive('sideNav',function(){
+   return {
+         scope: {
+             
+         }, 
+          replace:true,
+          template: 'Name'
+        };
+ });
